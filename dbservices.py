ecb0f04cc8cb924804657dbc7d0635dec2653eed
ken kivuti <kivutiken4@gmail.com>
2023-09-23 10:33:11 +0300

    change

--- dbservices.py
+++ dbservices.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def table_name(p):
    conn = psycopg2.connect(
-   database="mytest1", user='postgres', password='kenkivuti254', host='127.0.0.1', port= '5432')
+   database="dukaclass", user='postgres', password='kenkivuti254', host='127.0.0.1', port= '5432')
    cursor = conn.cursor()
    t = "select * from"+" " + p
    cursor.execute(t)
