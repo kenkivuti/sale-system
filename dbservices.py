2483cca633f965cc2c53ba5b52e7ab5b306e332e
ken kivuti <kivutiken4@gmail.com>
2023-09-25 17:34:53 +0300

    notactual

--- dbservices.py
+++ dbservices.py
@@ -1,8 +1,9 @@
 import psycopg2
 
+
+conn = psycopg2.connect(
+database="dukaclass", user='postgres', password='kenkivuti254', host='127.0.0.1', port= '5432')
 def table_name(p):
-   conn = psycopg2.connect(
-   database="dukaclass", user='postgres', password='kenkivuti254', host='127.0.0.1', port= '5432')
    cursor = conn.cursor()
    t = "select * from"+" " + p
    cursor.execute(t)
@@ -16,5 +17,40 @@ products=table_name("products")
 
 
 sales = table_name("sales")
-print(sales)
+# print(sales)
 
+
+# def input_data(pro):
+#    conn = psycopg2.connect(
+#    database="dukaclass", user='postgres', password='kenkivuti254', host='127.0.0.1', port= '5432')
+#    cursor = conn.cursor()
+#    t = "insert into  "+" " + pro + " (name,buying_price,selling_price,stock_quantity)values()"
+#    cursor.execute(t)
+#    data = cursor.fetchall()
+#    return data
+
+
+
+
+def insert_data():
+    try:
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(database="dukaclass", user="postgres", password="kenkivuti254", host="127.0.0.1", port="5432")
+        cur = conn.cursor()
+
+        # Execute the INSERT statement
+        cur.execute("INSERT INTO products (name, buying_price,selling_price,stock_quantity) VALUES (%s, %s)", ("yoghurt", "20","50","10"))
+
+        # Commit the transaction
+        conn.commit()
+
+        # Close the cursor and connection
+        cur.close()
+        conn.close()
+
+        print("Data inserted successfully!")  
+    except (Exception, psycopg2.Error) as error:
+            print("Error while inserting data:", error)
+
+total=insert_data()
+print(total)
